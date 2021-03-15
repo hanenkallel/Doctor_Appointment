fa31259d1968e4590522f30ab7a0e4ca60f7f0fd
unknown <hanenkallel15@gmail.com>
2021-03-15 21:01:32 +0100

    updateapplicationproperties

--- src/main/java/enet/project/appointment/AppointementApplication.java
+++ src/main/java/enet/project/appointment/AppointementApplication.java
@@ -1,5 +1,11 @@
 package enet.project.appointment;
 
+import javax.transaction.Transactional;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@Transactional
 public class AppointementApplication {
 
 	public static void main(String[] args) {
